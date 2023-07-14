b18afe1537bc8c80ff9d682e0a416e2d106d1d31
Sumit Bera <berasumit611@gmail.com>
2023-07-14 16:50:09 +0530

    14 july

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
+//!+++++++++++++ preloder +++++++++++++
+// Delay function
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Show the preloader initially
+var preloader = document.querySelector('.preloader');
+preloader.style.display = 'flex'; // Display the preloader
+
+// Delay for 5 seconds
+delay(500).then(function() {
+  // Hide the preloader and display the page content
+  preloader.style.display = 'none';
+  document.body.style.visibility = 'visible'; // Make the page content visible
+});
+
+// Hide the preloader when the page content is loaded
+// window.addEventListener('load', function() {
+//   var preloader = document.querySelector('.preloader');
+//   preloader.style.display = 'none';
+// });
+
+
+
+
+
 //!+++++++++++++ scroll to top functionality +++++++++++++
 
 
@@ -44,6 +71,19 @@ scroll_to_top_element.addEventListener("click",()=>{
 
 
 
+//!+++++++++++++ sticky header +++++++++++++
+// const hero_section=document.getElementById('hero');
+// const sticky_observer=new IntersectionObserver((entries)=>{
+//   const ent=entries[0];
+//   !ent.isIntersecting?document.body.classList.add("sticky"):document.body.classList.remove('sticky');
+// },
+// {
+//   root:null,
+//   threshold:0,
+// });
+
+// sticky_observer.observe(hero_section);
+
 
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
                            header section responcive
@@ -81,4 +121,39 @@ function handleWindowResize() {
 mobile_nav_btn.addEventListener("click", () => {
   header_element.classList.toggle("active");
 });
+
+
+// animated no section
+function animateNumbers() {
+  const elements = document.getElementsByClassName("counter-number");
+
+  for (let i = 0; i < elements.length; i++) {
+    const startValue = parseInt(elements[i].textContent);
+    const endValue = Math.floor(Math.random() * 100000000); // Set your desired end value here
+    const duration = 3000; // Set the animation duration in milliseconds
+
+    let startTime = null;
+
+    function step(timestamp) {
+      if (!startTime) startTime = timestamp;
+      const progress = timestamp - startTime;
+
+      const currentValue = Math.floor(
+        startValue + ((endValue - startValue) * progress) / duration
+      );
+
+      elements[i].textContent = currentValue.toLocaleString();
+
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      } else {
+        elements[i].textContent = endValue.toLocaleString();
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  }
+}
+
+animateNumbers();
 
