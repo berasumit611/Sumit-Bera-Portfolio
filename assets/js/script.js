7798ae8eae94ce810f138684bdeec934423c7d33
Sumit Bera <berasumit611@gmail.com>
2024-01-01 22:42:59 +0530

    codebase refactor 1st jan 2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,10 @@ const tech_stack_data=[
         img:`<img width="25" height="25" src="https://img.icons8.com/office/20/react.png" alt="react"/>`
     },
     {
+        name:"PHP",
+        img:`<img width="25" height="25" src="https://img.icons8.com/ios/25/php-logo.png" alt="php"/>`
+    },
+    {
         name:"Bootstrap",
         img:`<img width="25" height="25" src="https://img.icons8.com/color-glass/20/bootstrap.png" alt="bootstrap"/>`
     },
@@ -71,14 +75,14 @@ tech_stack_container.innerHTML=all_tech_element;
 //RENDER ALL EDUCATIONAL AND EXPERIENCE DATA
 const experience_data=[
     {
-        date:"may 2023 - june 2023",
+        date:"May 2023 - June 2023",
         job_role:"Web Development and Designing intern",
         company:"[Oasis Infobyte] - Remote",
         description:`Developed some mini    responsive web application using HTML, CSS, and JavaScript.
         Gained knowledge of frontend responsive web design.`
     },
     {
-        date:"may 2023 - june 2023",
+        date:"May 2023 - June 2023",
         job_role:"Web Development intern",
         company:"[InternPe] - Remote",
         description:`Gained knowledge of frontend responsive web design.
@@ -87,27 +91,27 @@ const experience_data=[
 ]
 const education_data=[
     {
-        date:"january 2023 - present",
-        institution:"FreeCodeCamp udemy & coursera",
+        date:"January 2023 - Present",
+        institution:"Self learning",
         description:`Take  a bunch of Courses.
         On web development, javascript, php and pogramming languages.Continiously upskilling myself with latest technologies.`
     },
     {
-        date:"2020 - present",
+        date:"2020 - Present",
         institution:"MCKV Institute of Engineering, Liluah [B.Tech]",
         description:`University - Maulana Abul Kalam Azad
         University of Technology
-        <br>currently in 7th semester.
-        <br>Stream - Electrical Engineering | CGPA - 8.8/10`
+        <br>currently in 8th semester.
+        <br>Stream - Electrical Engineering | CGPA - 8.67/10`
     },
     {
-        date:"2019 complited",
-        institution:`garalgacha high school, Chanditala 
+        date:"2019 Complited",
+        institution:`Garalgacha high school, Chanditala 
         <br>
         (Primary and higher secondary education)`,
         description:`Higher secondary exam (WBCHSE board) | Score - 73% | Stream - Science
         <br>
-       higher secondary exam (WBBSE board) | Score - 77.85%`
+       Higher secondary exam (WBBSE board) | Score - 77.85%`
     }
 ]
 
@@ -127,7 +131,7 @@ function render_experience_and_education_data(){
                                     ${data.company}</h3>
         
                                 <p>
-                                    <i class="fa-solid fa-bullseye" style="color: #4b65ce;"></i> ${data.description}
+                                <img width="15" height="15" src="https://img.icons8.com/pastel-glyph/20/define-location--v2.png" alt="define-location--v2"/> ${data.description}
                                 </p>
                             </div>`;
 
@@ -146,7 +150,7 @@ function render_experience_and_education_data(){
         
                                 <h3>${data.institution}</h3>
        
-                                <p><i class="fa-solid fa-bullseye" style="color: #4b65ce;"></i>
+                                <p> <img width="15" height="15" src="https://img.icons8.com/pastel-glyph/20/define-location--v2.png" alt="define-location--v2"/> 
                                 ${data.description}
                                 </p>
         
